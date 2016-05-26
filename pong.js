e4b42c6dde8a876d6a135eef9f4e06d95a4a959a
Mayowa <mayowaadegbola.r@gmail.com>
2016-05-26 16:02:31 +0100

    add ball control and game scoring system

--- pong.js
+++ pong.js
@@ -1,35 +1,25 @@
+<html>
+
+<canvas id="gameCanvas" width="800" height="600"></canvas>
+
+<script>
 var canvas;
 var canvasContext;
 var ballX = 50;
 var ballY = 50;
-var ballSpeedX = 3;
+var ballSpeedX = 10;
 var ballSpeedY = 4;
 
 var player1Score = 0;
 var player2Score = 0;
+const WINNING_SCORE = 3;
+
+var showingWinScreen = false;
 
 var paddle1Y = 250;
 var paddle2Y = 250;
 const PADDLE_THICKNESS = 10;
 const PADDLE_HEIGHT = 100;
-
-
-window.onload = function() {
-	canvas = document.getElementById('gameCanvas');
-	canvasContext = canvas.getContext('2d');
-
-	var framesPerSecond = 60;
-	setInterval(function() {
-			moveEverything();
-			drawEverything();	
-		}, 1000/framesPerSecond);
-
-	canvas.addEventListener('mousemove',
-		function(evt) {
-			var mousePos = calculateMousePos(evt);
-			paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
-		});
-}
 
 function calculateMousePos(evt) {
 	var rect = canvas.getBoundingClientRect();
@@ -42,7 +32,33 @@ function calculateMousePos(evt) {
 	};
 }
 
+window.onload = function() {
+	canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d');
+
+	var framesPerSecond = 30;
+	setInterval(function() {
+			moveEverything();
+			drawEverything();	
+		}, 1000/framesPerSecond);
+
+	canvas.addEventListener('mousemove',
+		function(evt) {
+			var mousePos = calculateMousePos(evt);
+			paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
+		});
+}
+
 function ballReset() {
+	if(player1Score >= WINNING_SCORE ||
+		player2Score >= WINNING_SCORE) {
+
+		player1Score = 0;
+		player2Score = 0;
+		showingWinScreen = true;
+
+	}
+
 	ballSpeedX = -ballSpeedX;
 	ballX = canvas.width/2;
 	ballY = canvas.height/2;
@@ -58,6 +74,10 @@ function computerMovement() {
 }
 
 function moveEverything() {
+	if(showingWinScreen) {
+		return;
+	}
+
 	computerMovement();
 
 	ballX = ballX + ballSpeedX;
@@ -67,18 +87,26 @@ function moveEverything() {
 		if(ballY > paddle1Y &&
 			ballY < paddle1Y+PADDLE_HEIGHT) {
 			ballSpeedX = -ballSpeedX;
+
+			var deltaY = ballY
+					-(paddle1Y+PADDLE_HEIGHT/2);
+			ballSpeedY = deltaY * 0.35;
 		} else {
+			player2Score++; // must be BEFORE ballReset()
 			ballReset();
-			player2Score++;
 		}
 	}
 	if(ballX > canvas.width) {
 		if(ballY > paddle2Y &&
 			ballY < paddle2Y+PADDLE_HEIGHT) {
 			ballSpeedX = -ballSpeedX;
+
+			var deltaY = ballY
+					-(paddle2Y+PADDLE_HEIGHT/2);
+			ballSpeedY = deltaY * 0.35;
 		} else {
+			player1Score++; // must be BEFORE ballReset()
 			ballReset();	
-			player1Score++;
 		}
 	}
 	if(ballY < 0) {
@@ -92,6 +120,12 @@ function moveEverything() {
 function drawEverything() {
 	// next line blanks out the screen with black
 	colorRect(0,0,canvas.width,canvas.height,'black');
+
+	if(showingWinScreen) {
+		canvasContext.fillStyle = 'white';
+		canvasContext.fillText("click to continue", 100, 100);
+		return;
+	}
 
 	// this is left player paddle
 	colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
@@ -117,3 +151,7 @@ function colorRect(leftX,topY, width,height, drawColor) {
 	canvasContext.fillStyle = drawColor;
 	canvasContext.fillRect(leftX,topY, width,height);
 }
+
+</script>
+
+</html>
