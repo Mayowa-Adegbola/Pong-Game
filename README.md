e40c041c647aafd14c8cd3c76d103683ba2f614a
Mayowa <mayowaadegbola.r@gmail.com>
2016-05-27 09:08:51 +0100

    Add CSS stylings to html5 canvas

---
+++ README.md
@@ -0,0 +1,18 @@
+# Pong Game
+
+This app is a recreation of the classical Pong game by Atari Inc.
+
+## Functionalities
+
+1. Responsive ball control
+2. Computer Opponent
+
+## Instructions
+
+1. Visit http://pong.abdulmujeebjamiu.com/
+2. Play till your fingers are numb!
+
+## Credits
+
+1. Andela Bootcamp Trainers
+2. Bootcampers
--- pong.js
+++ pong.js
@@ -1,13 +1,13 @@
 var canvas;
 var canvasContext;
-var ballX = 50;
-var ballY = 50;
-var ballSpeedX = 10;
-var ballSpeedY = 4;
+var ballX = 30;
+var ballY = 30;
+var ballSpeedX = 5;
+var ballSpeedY = 1;
 
 var player1Score = 0;
 var player2Score = 0;
-const WINNING_SCORE = 3;
+const WINNING_SCORE = 11;
 
 var showingWinScreen = false;
 
@@ -15,13 +15,18 @@ var paddle1Y = 250;
 var paddle2Y = 250;
 const PADDLE_THICKNESS = 10;
 const PADDLE_HEIGHT = 100;
+var collision = null;
+var finish = null;
 
 window.onload = function() {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
+	collision = document.getElementById("collide");
+	finish = document.getElementById("finish");
 
-	var framesPerSecond = 30;
-	setInterval(function() {
+	var framesPerSecond = 60;
+
+  	setInterval(function() {
 			moveEverything();
 			drawEverything();	
 		}, 1000/framesPerSecond);
@@ -39,6 +44,7 @@ function ballReset() {
 	if(player1Score >= WINNING_SCORE ||
 		player2Score >= WINNING_SCORE) {
 
+		finish.play();
 		showingWinScreen = true;
 
 	}
@@ -67,35 +73,38 @@ function moveEverything() {
 	ballX = ballX + ballSpeedX;
 	ballY = ballY + ballSpeedY;
 	
-	if(ballX < 0) {
-		if(ballY > paddle1Y &&
-			ballY < paddle1Y+PADDLE_HEIGHT) {
-			ballSpeedX = -ballSpeedX;
+	if(ballX < 15) {
+		if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT) {
+			
 
-			var deltaY = ballY
-					-(paddle1Y+PADDLE_HEIGHT/2);
-			ballSpeedY = deltaY * 0.35;
+			ballSpeedX = -ballSpeedX;
+			collision.play();
+			var deltaY = ballY-(paddle1Y+PADDLE_HEIGHT/2);
+			ballSpeedY = deltaY * 0.10;
+
 		} else {
 			player2Score++; 
 			ballReset();
 		}
 	}
-	if(ballX > canvas.width) {
-		if(ballY > paddle2Y &&
-			ballY < paddle2Y+PADDLE_HEIGHT) {
-			ballSpeedX = -ballSpeedX;
 
-			var deltaY = ballY
-					-(paddle2Y+PADDLE_HEIGHT/2);
-			ballSpeedY = deltaY * 0.35;
+	if(ballX > (canvas.width - 15) ) {
+		if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT) {
+			
+			ballSpeedX = -ballSpeedX;
+			collision.play();
+			var deltaY = ballY-(paddle2Y+PADDLE_HEIGHT/2);
+			ballSpeedY = deltaY * 0.10;
 		} else {
 			player1Score++; 
 			ballReset();	
 		}
 	}
+
 	if(ballY < 0) {
 		ballSpeedY = -ballSpeedY;
 	}
+
 	if(ballY > canvas.height) {
 		ballSpeedY = -ballSpeedY;
 	}
@@ -109,37 +118,39 @@ function drawNet() {
 
 function drawEverything() {
 	// next line blanks out the screen with black
-	colorRect(0,0,canvas.width,canvas.height,'black');
+	colorRect(0,0,canvas.width,canvas.height, "#0a181c");
 
 	if(showingWinScreen) {
 		canvasContext.fillStyle = 'white';
 
 		if(player1Score >= WINNING_SCORE) {
-			canvasContext.fillText("Left Player Won", 350, 200);
+			;
+			canvasContext.fillText("You won!", 250, 200);
 		} else if(player2Score >= WINNING_SCORE) {
-			canvasContext.fillText("Right Player Won", 350, 200);
+			canvasContext.font = "50px serif"
+			canvasContext.fillText("Computer won!", 250, 200);
 		}
-
-		canvasContext.fillText("click to continue", 350, 500);
+		canvasContext.font = "25px serif"
+		canvasContext.fillText("Click anywhere to restart", 300, 500);
 		return;
 	}
 
 	drawNet();
 
 	// this is left player paddle
-	colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
+	colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'brown');
 
 	// this is right computer paddle
-	colorRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
+	colorRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'brown');
 
 	// next line draws the ball
 	colorCircle(ballX, ballY, 10, 'white');
-
+	canvasContext.font = "35px serif"
 	canvasContext.fillText(player1Score, 100, 100);
 	canvasContext.fillText(player2Score, canvas.width-100, 100);
 }
 
-function colorCircle(centerX, centerY, radius, drawColor) {
+function colorCircle(centerX, centerY, radius, drawColor, fontType) {
 	canvasContext.fillStyle = drawColor;
 	canvasContext.beginPath();
 	canvasContext.arc(centerX, centerY, radius, 0,Math.PI*2,true);
@@ -150,7 +161,6 @@ function colorRect(leftX,topY, width,height, drawColor) {
 	canvasContext.fillStyle = drawColor;
 	canvasContext.fillRect(leftX,topY, width,height);
 }
-
 
 function calculateMousePos(evt) {
 	var rect = canvas.getBoundingClientRect();
@@ -170,3 +180,4 @@ function handleMouseClick(evt) {
 		showingWinScreen = false;
 	}
 }
+
